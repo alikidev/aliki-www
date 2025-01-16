0ab5ab6e1ee6e77848390787dadca53984a6e9f9
alkinoosmat <alkinoosmatr@gmail.com>
2025-01-16 20:48:10 +0200

    rmv unused files

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import './globals.css';
 import { Inter } from 'next/font/google';
 import { EXAMPLE_PATH, CMS_NAME } from '@/lib/constants';
 import Footer from '../components/footer';
-import { NavigationMenu } from '@/components/ui/navigation-menu';
 import Menu from '../components/menu';
 export const metadata = {
 	title: `Next.js and ${CMS_NAME} Example`,
