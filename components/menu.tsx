d39cad74eaf1a3d2d56c0885a742c4c4d0a7f580
alkinoosmat <alkinoosmatr@gmail.com>
2025-02-13 17:48:29 +0200

    update menu logo image height to improve display consistency

--- components/menu.tsx
+++ components/menu.tsx
@@ -85,7 +85,7 @@ export default function Menu() {
 						alt='Delivoria Therapy Logo'
 						width={180}
 						height={60}
-						className='h-24 w-auto'
+						className='h-22 w-auto'
 						loader={customImageLoader}
 						priority
 					/>
@@ -188,7 +188,7 @@ export default function Menu() {
 								alt='Delivoria Therapy Logo'
 								width={180}
 								height={60}
-								className='h-20 w-auto'
+								className='h-22 w-auto'
 								loader={customImageLoader}
 								priority
 							/>
